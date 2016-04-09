7e52770a6b3e3405ff385e7366103314f29fcc15
bibi21000 <bibi21000@gmail.com>
2016-04-09 07:40:20 +0200

    Initial import

---
+++ tests/test_docker.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+"""Unittests for Janitoo-common.
+"""
+__license__ = """
+    This file is part of Janitoo.
+
+    Janitoo is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Janitoo is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Janitoo. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+__author__ = 'Sébastien GALLET aka bibi21000'
+__email__ = 'bibi21000@gmail.com'
+__copyright__ = "Copyright © 2013-2014-2015-2016 Sébastien GALLET aka bibi21000"
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import sys, os
+import time
+import unittest
+import logging
+import threading
+import mock
+import logging
+
+from janitoo_nosetests import JNTTBase
+from janitoo_nosetests.server import JNTTDockerServerCommon, JNTTDockerServer
+
+from janitoo.runner import Runner, jnt_parse_args
+from janitoo.server import JNTServer
+from janitoo.utils import HADD_SEP, HADD
+
+from janitoo_raspberry.server import PiServer
+
+class TestI2CSerser(JNTTDockerServer, JNTTDockerServerCommon):
+    """Test the server
+    """
+    loglevel = logging.DEBUG
+    path = '/tmp/janitoo_test'
+    broker_user = 'toto'
+    broker_password = 'toto'
+    server_class = PiServer
+    server_conf = "tests/data/janitoo_raspberry_i2c.conf"
+    hadds = [HADD%(159,0), HADD%(159,1), HADD%(159,2), HADD%(159,3), HADD%(159,4), ]
+
+    def test_030_wait_for_all_nodes(self):
+        self.skipRasperryTest()
+        if self.hadds is None:
+            self.skipTest("No hadds defined. Skip test")
+        self.start()
+        try:
+            self.assertHeartbeatNodes(hadds=self.hadds)
+        finally:
+            self.stop()
